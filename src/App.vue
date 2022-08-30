3ec9f5ed90f5ef7307c79b93634eeb510477e2f5
amranhakim10 <amranhakim9@gmail.com>
2022-08-30 23:14:48 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <Login msg="Login"/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
-
-<script>
-import Login from './components/Login.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Login
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
